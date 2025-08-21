fa4df93bd93d4b219ff63acd22d581cad84393e1
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-08-21 22:31:25 +0530

    update

---
+++ my_dag.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG("hello_kube",
+         start_date=datetime(2023,1,1),
+         schedule_interval=None,
+         catchup=False) as dag:
+
+    task = KubernetesPodOperator(
+        namespace="default",
+        image="busybox",
+        cmds=["echo"],
+        arguments=["hello from k3s"],
+        labels={"test": "hello"},
+        name="hello-kube",
+        task_id="hello_kube_task",
+        get_logs=True
+    )
